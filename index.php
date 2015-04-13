1c4def230672982032e904f4ca1f936d4adf9467
isaacrdz <zack_cpp@hotmail.com>
2015-04-13 17:39:48 -0500

    mail backend was added

--- index.php
+++ index.php
@@ -26,45 +26,45 @@
 
 
 
-	<link rel="stylesheet" href="foundation/css/foundation.css" />
+  <link rel="stylesheet" href="foundation/css/foundation.css" />
     <script src="foundation/js/vendor/modernizr.js"></script>
 </head>
 <body>
 
 
-	
-	<p><img class="img-center" data-interchange="[img/auto.png, (small)],img/auto.png, img/auto.png"></p>
-	<noscript><img class="img-center" src="img/auto.png"></noscript>
+  
+  <p><img class="img-center" data-interchange="[img/auto.png, (small)],img/auto.png, img/auto.png"></p>
+  <noscript><img class="img-center" src="img/auto.png"></noscript>
 
 
 <section>
-	<div class="row">
-		<div class="small-12 medium-12 large-12 columns">
-			<h3 class="color-yellow">Solicita información</h3>
-			<form>
+  <div class="row">
+    <div class="small-12 medium-12 large-12 columns">
+      <h3 class="color-yellow">Solicita cotización</h3>
+      <form>
   <div class="row">
     <div class="large-12 columns">
       <label>Nombre
-        <input type="text" placeholder="Nombre" required />
+        <input type="text" placeholder="Nombre" name="nombre" required />
       </label>
     </div>
   </div>
   <div class="row">
     <div class="medium-4 large-4 columns">
       <label>Correo
-        <input type="email" placeholder="@" required/>
+        <input type="email" placeholder="@" name="correo" required/>
       </label>
     </div>
     <div class="medium-4 large-4 columns">
       <label>Teléfono
-        <input type="email" placeholder="10 dígitos"required />
+        <input type="tel" placeholder="10 dígitos" name="telefono"  required />
 
       </label>
     </div>
 
     <div class="medium-4 large-4 columns">
       <label>Auto
-        <select>
+        <select name="seleccion">
           <option class="disabled" value="Opción">-Escoge una opción-</option>        
           <option value="Aveo">Aveo</option>
           <option value="Matiz">Matiz</option>
@@ -80,14 +80,14 @@
   <div class="row">
     <div class="large-12 columns">
       <label>Mensaje
-        <textarea placeholder="Mensaje"></textarea>
+        <textarea placeholder="Mensaje" name="mensaje"></textarea>
       </label>
  <button class=" right" type="submit">Enviar</button>
 </div>
   </div>
 </form>
-		</div>
-	</div>
+    </div>
+  </div>
 </section>
 <div class="row">  
       <div
@@ -99,29 +99,29 @@
 </div>
 
 <style>
-	#flat
-	{
-		background-color: #289dcc;
-	}
-	
-	.color-1
-	{
-		color: white;
-	}
-	footer
-	{
-		background-color: #1e2126;
-	}
-	.padding-1
-	{
-		padding: 1em;
-	}
+  #flat
+  {
+    background-color: #289dcc;
+  }
+  
+  .color-1
+  {
+    color: white;
+  }
+  footer
+  {
+    background-color: #1e2126;
+  }
+  .padding-1
+  {
+    padding: 1em;
+  }
 
-	#galeria
-	{
-		background:#1e2126; 
-		margin: 1em 0;
-	}
+  #galeria
+  {
+    background:#1e2126; 
+    margin: 1em 0;
+  }
 
   .color-yellow
   { 
@@ -129,8 +129,8 @@
     font-weight: bold;
   }
 </style>
-<!-- 	end flat section -->
-	 <script src="foundation/js/vendor/jquery.js"></script>
+<!--  end flat section -->
+   <script src="foundation/js/vendor/jquery.js"></script>
     <script src="foundation/js/foundation.min.js"></script>
     <script>
       $(document).foundation();
